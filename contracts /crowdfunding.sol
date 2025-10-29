e3cee3abd921396942580fbd6c201bb9e002ae34
lavishamali <lavishamali5@gmail.com>
2025-10-29 20:28:25 +0530

    Update crowdfunding.sol

--- contracts /crowdfunding.sol
+++ contracts /crowdfunding.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Crowdfunding {
+    address public owner;
+    uint256 public goal;
+    uint256 public totalFunds;
+
+    constructor(uint256 _goal) {
+        owner = msg.sender;
+        goal = _goal;
+    }
+
+    // Function to contribute to the campaign
+    function contribute() public payable {
+        require(msg.value > 0, "Contribution must be greater than 0");
+        totalFunds += msg.value;
+    }
+
+    // Function to withdraw funds by owner if goal is met
+    function withdraw() public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        require(totalFunds >= goal, "Funding goal not reached");
+        payable(owner).transfer(address(this).balance);
+    }
+}
 
